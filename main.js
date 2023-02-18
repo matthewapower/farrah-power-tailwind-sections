f03c18bda3746a8ba2c65d539dd4528e4961bf5d
Matt Power <matthewallenpower@gmail.com>
2023-02-18 10:19:15 -0800

    Update to use inline alpine vs JS

--- main.js
+++ main.js
@@ -1,26 +1,26 @@
 import './style.css'
-import Alpine from 'alpinejs'
- 
-window.Alpine = Alpine
+// import Alpine from 'alpinejs'
+//  
+// window.Alpine = Alpine
+//
+// Alpine.start()
 
-Alpine.start()
-
-let lastScrollPos = window.scrollY
-window.lastHeaderState = 'top'
-
-document.addEventListener('scroll', () => {
-  const header = document.querySelector('.header')
-  const scrollPos = window.scrollY
-  const scrollDir = lastScrollPos < scrollPos ? 'down' : 'up'
-  let state = 'top'
-
-
-  if (scrollDir == 'down' && scrollPos > 40) state = 'off-top'
-  if (scrollDir == 'down' && scrollPos > 1000) state = 'content'
-  if (scrollDir == 'up') state = 'up'
-  if (scrollPos <= 40) state = 'top'
-
-  if (state != lastHeaderState) header.setAttribute('state', state)
-  lastScrollPos = scrollPos
-  window.lastHeaderState = state
-})
+// let lastScrollPos = window.scrollY
+// window.lastHeaderState = 'top'
+//
+// document.addEventListener('scroll', () => {
+//   const header = document.querySelector('.header')
+//   const scrollPos = window.scrollY
+//   const scrollDir = lastScrollPos < scrollPos ? 'down' : 'up'
+//   let state = 'top'
+//
+//
+//   if (scrollDir == 'down' && scrollPos > 40) state = 'off-top'
+//   if (scrollDir == 'down' && scrollPos > 1000) state = 'content'
+//   if (scrollDir == 'up') state = 'up'
+//   if (scrollPos <= 40) state = 'top'
+//
+//   if (state != lastHeaderState) header.setAttribute('state', state)
+//   lastScrollPos = scrollPos
+//   window.lastHeaderState = state
+// })
